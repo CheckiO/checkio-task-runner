154a2b311e221e67ef2993aab28865a88c05ff09
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-07-02 16:41:11 +1200

    Add task template and static files

--- checkio-local-task.py
+++ checkio-local-task.py
@@ -1,13 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, json
 import sys
 import os
 
 app = Flask(__name__)
-TASK_PATH = ""
+TASK_PATH = "./"
+DEFAULT_NAME = "Task's Name"
+
+
+def error_page(message, er):
+    context = {
+        "message": message
+    }
+    return render_template("error.html", **context)
 
 @app.route('/')
 def main_page():
-    return 'Hello World!'
+    global TASK_PATH
+    try:
+        description = open(os.path.join(TASK_PATH, "description.html")).read()
+    except IOError as er:
+        return error_page("Cant find the description file.", er)
+    try:
+        task_config_file = open(os.path.join(TASK_PATH, "task.json"))
+        task_config = json.load(task_config_file)
+    except IOError as error:
+        return error_page("Cant find the task's config file.", error)
+    context = {
+        'task_name': task_config.get('title', DEFAULT_NAME),
+        'description': description.decode("utf-8"),
+    }
+    return render_template("task.html", **context)
 
 
 if __name__ == '__main__':
@@ -17,6 +39,7 @@ if __name__ == '__main__':
         sys.exit(1)
     global TASK_PATH
     TASK_PATH = sys.argv[1]
+    print(TASK_PATH)
     if not os.path.exists(TASK_PATH):
         print("Cant find the folder")
         sys.exit(2)
