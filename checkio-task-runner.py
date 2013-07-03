8fe7b0c15d73bc6ee94632c6ad6f6db90a663b91
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-07-03 16:52:58 +1200

    Add explanation page redirect

--- checkio-task-runner.py
+++ checkio-task-runner.py
@@ -3,10 +3,10 @@ import sys
 import os
 import traceback
 
-app = Flask(__name__, static_folder="./static-local")
 TASK_PATH = "./"
-DEFAULT_NAME = "Task's Name"
+TASK_NAME = "Task's Name"
 
+app = Flask(__name__, static_folder="./static-local")
 
 @app.route('/static/files/illustrations/<path:filename>')
 def custom_static_illustrations(filename):
@@ -17,31 +17,62 @@ def custom_static_icon(filename):
     return send_from_directory(os.path.join(TASK_PATH, "icon"), filename)
 
 
-def error_page(message, er, trace=None):
+def error_page(message, trace=""):
     context = {
         "message": message,
-        "traceback": trace
+        "traceback": trace,
+        "task_name": TASK_NAME
+
     }
     return render_template("error.html", **context)
 
 @app.route('/')
 def main_page():
     global TASK_PATH
+    global TASK_NAME
     try:
         description = open(os.path.join(TASK_PATH, "description.html")).read()
     except IOError as er:
         trace = traceback.format_exc()
-        return error_page("Cant find the description file.", er, trace)
+        return error_page("Cant find the description file.", trace)
     try:
         task_config_file = open(os.path.join(TASK_PATH, "task.json"))
         task_config = json.load(task_config_file)
     except IOError as error:
         return error_page("Cant find the task's config file.", error)
+    TASK_NAME = task_config.get('title', TASK_NAME)
     context = {
-        'task_name': task_config.get('title', DEFAULT_NAME),
+        'task_name': TASK_NAME,
         'description': description.decode("utf-8"),
     }
     return render_template("task.html", **context)
+
+@app.route("/explanation")
+@app.route("/explanation/<string:category>/<int:number>")
+def test_explanation(category=None, number=None):
+    test_dir = os.path.join(TASK_PATH, "tests")
+    if not os.path.exists(test_dir):
+        return error_page("Cant find tests folder.")
+    categories = [p.split("_", 1)[1].rsplit(".", 1)[0]
+                  for p in os.listdir(test_dir)
+                  if p.startswith("test_") and p.endswith(".json")]
+    categories = filter(None, categories)
+    if not categories:
+        return error_page("Cant find any tests files.")
+    print(category)
+    if not category:
+        print(categories)
+        return redirect("/explanation/{0}/1".format(categories[0]))
+
+    context = {
+        'task_name': TASK_NAME
+    }
+
+    return render_template("explanation.html", **context)
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -54,4 +85,5 @@ if __name__ == '__main__':
     if not os.path.exists(TASK_PATH):
         print("Cant find the folder")
         sys.exit(2)
+    app.debug = True
     app.run()
