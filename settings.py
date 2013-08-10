69c63fefbe9dec215d91c85ecaa4cc66bd8f9a84
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-08-10 21:43:13 +1200

    comments for settings

--- settings.py
+++ settings.py
@@ -1,27 +1,60 @@
 #Runner settings
+#===============================================
+#port for main web server
 WEB_PORT = 5000
+
+#dir with templates for runner
 TEMPLATE_DIR = 'templates'
+
+#static dir for runner
 STATIC_DIR = 'static'
+
+#dir for temporary files
 TEMP_DIR = "./"
+#temporary code file
 TEMP_CODE = "temporary.py"
 
+#address for main runner server
 CENTER_IP = "127.0.0.1"
+#runner address
 CENTER_PORT = 2324
 
+#url for media
+MEDIA_URL = 'media'
+
+#===============================================
 
 #Task's structure settings
+#===============================================
+#Main config file for task
 TASK_CONFIG_NAME = "task.json"
+
+#dir with task descriptions
 INFO_DIR = 'info'
-EDITOR_DIR = 'editor'
-VERIFICATION_DIR = 'verification'
-TESTS_DIR = 'tests'
-REFEREE_NAME = 'referee.py'
+#media file directory
 MEDIA_DIR = 'media'
-MEDIA_URL = 'media'
-DESCRIPTION_FILE_NAME = 'task_description.html'
-STORY_FILE_NAME = 'story.html'
+#dir for logos
 LOGO_DIR = 'logo'
+#main description of task file
+DESCRIPTION_FILE_NAME = 'task_description.html'
+#file with story (not necessarily
+STORY_FILE_NAME = 'story.html'
+
+#dir for editor part (animation)
+EDITOR_DIR = 'editor'
+#dir with initial code files
 INITIAL_CODE_DIR = 'initial_code'
+#JS file for animation describe (tryit and explanation)
 ANIMATION_JS = 'animation.js'
+#css for animation
 ANIMATION_CSS = 'animation.css'
+#html template for animation
 TEMPLATE_HTML = 'template.html'
+
+#dir for checking part (tests, referee)
+VERIFICATION_DIR = 'verification'
+#dir for tests under verification
+TESTS_DIR = 'tests'
+#referee file name
+REFEREE_NAME = 'referee.py'
+#===============================================
